05ba2782d1c0d3ac247866bcf80d05907e03e258
Kevin Wilson <kevinw@interlink.com>
2022-11-17 08:35:00 -0500

    initial

---
+++ LC415/mainPackage/main.py
@@ -0,0 +1,12 @@
+# main.py 
+
+# Entry ponit to the test code
+# We will test the soltuion class
+# For LeetCode problem 415
+# The solution has been provided by ...
+
+from solutionPackage.Solution import * 
+
+# Instantiate an object of type solution 
+
+mySolution = Solution()
